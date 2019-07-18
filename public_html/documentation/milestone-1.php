8e116bffee4d56425064dcb6c778d30fe9dec8a7
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-07-17 20:51:11 -0600

    added milestone 1 and image penelope planner

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -22,3 +22,46 @@
 			<p>The primary goal if this site is to showcase the product M is for Macaron provide and to increase </p>
 			<p> their sales. the secondary goal is to showcase my work as a web developer.</p>
 		</div>
+		<div>
+			<h2>Persona</h2>
+			<h3>Penelope Planner</h3>
+			<img src="penelope-planner.jpg" alt="Penelope Planner">
+			<br>
+			<h4></h4>
+			<h4>Likes</h4>
+			<div>
+				<ul>
+					<li>Trendy coffee shops</li>
+					<li>introducing people to new things</li>
+					<li>Aesthetics</li>
+					<li>Expensive bags</li>
+					<li>Fashion</li>
+					<li>Dip nails</li>
+					<li>Face masks</li>
+			</div>
+			<h4>Dislikes</h4>
+			<div>
+				<ul>
+					<li>Tacky</li>
+					<li>Cheap</li>
+					<li>Careless work</li>
+					<li></li>
+				</ul>
+			</div>
+			<br>
+			<h4>Attitudes and Behaviors</h4>
+			<p>Always volunteering to plan parties for friends and family. short on time always because she over commits </p>
+			<p>herself. She will spend extra money for a better product or a job done right. Uses Instagram a lot, posts </p>
+			<p>decoration and food from her events. </p>
+			<br>
+			<h4>Technology</h4>
+			<p>always has the newest Iphone</p>
+			<p>MacBook Pro laptop</p>
+			<p>Apple watch</p>
+			<p>Instagram</p>
+			<p>twitter</p>
+			<p>pinterest</p>
+			<h4></h4>
+		</div>
+	</body>
+</html>
