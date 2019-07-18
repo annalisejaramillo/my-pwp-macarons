1986d5270c74e40a54e1d907174c899adb0d0f5e
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-07-17 22:55:40 -0600

    added persona information

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,24 +11,31 @@
 		<br>
 		<div>
 			<h2>Purpose, Audience, Goal</h2>
+			<br>
 			<h3>Purpose</h3>
-			<p>This project is to serve as a business website for M is for Macaron.</p>
+			<p>This project is to serve as a business website for &quot;M is for Macaron&quot;.</p>
 			<br>
 			<h3>Audience</h3>
-			<p>This website is designed with customers in mind, and is targeted to individuals in the event business, </p>
-			<p>the target audience is to include, wedding planners, party planners, dessert lovers</p>
+			<p>This website is designed with customers in mind, and is targeted to individuals in the event business and </p>
+			<p>those who have special occasions. the target audience is to include, wedding planners, party planners, dessert lovers</p>
 			<br>
 			<h3>Goal</h3>
-			<p>The primary goal if this site is to showcase the product M is for Macaron provide and to increase </p>
+			<p>The primary goal if this site is to showcase the product &quot;M is for Macaron&quot; provides and to increase </p>
 			<p> their sales. the secondary goal is to showcase my work as a web developer.</p>
 		</div>
+		<br>
 		<div>
 			<h2>Persona</h2>
 			<h3>Penelope Planner</h3>
+			<p>Age: 35</p>
+			<p>Work: Paralegal</p>
+			<p>Income: 45,000 </p>
+			<p>Relationship Status: engaged</p>
+			<br>
 			<img src="penelope-planner.jpg" alt="Penelope Planner">
 			<br>
-			<h4></h4>
 			<h4>Likes</h4>
+
 			<div>
 				<ul>
 					<li>Trendy coffee shops</li>
@@ -38,30 +45,49 @@
 					<li>Fashion</li>
 					<li>Dip nails</li>
 					<li>Face masks</li>
+					<li>Organization</li>
+				</ul>
 			</div>
+
 			<h4>Dislikes</h4>
 			<div>
 				<ul>
 					<li>Tacky</li>
 					<li>Cheap</li>
 					<li>Careless work</li>
-					<li></li>
+					<li>Inconsiderate people</li>
+					<li>Buffets</li>
+					<li>Germs</li>
+					<li>Dirt</li>
+					<li>Pet hair</li>
 				</ul>
 			</div>
-			<br>
-			<h4>Attitudes and Behaviors</h4>
-			<p>Always volunteering to plan parties for friends and family. short on time always because she over commits </p>
-			<p>herself. She will spend extra money for a better product or a job done right. Uses Instagram a lot, posts </p>
-			<p>decoration and food from her events. </p>
+
 			<br>
 			<h4>Technology</h4>
-			<p>always has the newest Iphone</p>
+			<p>Has the newest Iphone</p>
 			<p>MacBook Pro laptop</p>
 			<p>Apple watch</p>
 			<p>Instagram</p>
-			<p>twitter</p>
-			<p>pinterest</p>
-			<h4></h4>
+			<p>Twitter</p>
+			<p>Pinterest</p>
+			<br>
+			<h4>Attitudes and Behaviors</h4>
+			<p>Always volunteering to plan parties for friends and family. Always short on time because she&apos;s over committed. </p>
+			<p>She will spend extra money for a better product or a job done right. Uses Instagram a lot, to post </p>
+			<p>decoration and food from parties she throws. </p>
+			<br>
+			<h4>Frustrations and Needs</h4>
+			<p>Penelope needs a dessert for her party, she wants something uniques and elegant. She also wants to work with</p>
+			<p>someone who will easily understand her needs. She hates having to over explain herself but wants </p>
+			<p>her order done exactly right.</p>
+			<br>
+			<h4>Goal</h4>
+			<p>Penelope&apos; only goal is to find a macaron business that meets her standerds. she likes thing to be </p>
+			<p>aestheticly pleasing and trendy and she&apos;s looking for that in business branding as well as product.</p>
+			<br>
+			<h4>User Story</h4>
+			<p>As a user I want to be able to decide quickly if this is the dessert im looking for for my party</p>
 		</div>
 	</body>
 </html>
